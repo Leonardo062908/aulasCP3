5c4a1ec4db9e67f9657aeedcca5ca6c161cadc7c
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-08-08 22:35:00 -0300

    feat: criação de entidades + exceções

--- spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
+++ spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
@@ -1,18 +1,94 @@
 package br.com.fiap.spring_mvc.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+import java.math.BigDecimal;
 
 @Entity
 public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "O titulo é obrigatório!")
     private String titulo;
-    private String Autor;
+    @NotBlank(message = "O autor é obrigatório")
+    private String autor;
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "A categoria é obrigatório")
     private Categoria categoria;
+    private String editora;
+    @DecimalMin(value = "0.99", message = "O preço deve ser no mínimo 0.99")
+    private BigDecimal preco;
+    @Pattern(regexp = "^970\\d(7)$|^970\\d{10}$",
+            message = "ISBN fora do padrão")
+    private String isbn;
+    private LocalDate dataPublicacao;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
 }
