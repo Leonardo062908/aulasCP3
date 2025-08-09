b28c2f4034b17123266bea7427772c3f77b31d14
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-08-08 21:57:32 -0300

    feat: criação do projeto Spring Boot

---
+++ spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
@@ -0,0 +1,18 @@
+package br.com.fiap.spring_mvc.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String Autor;
+    private Categoria categoria;
+
+}
