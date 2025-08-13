ff9313432425cb741a5b1a498a5b64a0e3694efc
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-08-12 23:00:19 -0300

    feat: avanço em aula 12/08

--- spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
+++ spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/entity/Livro.java
@@ -1,29 +1,27 @@
 package br.com.fiap.spring_mvc.entity;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
-import java.time.LocalDate;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "O titulo é obrigatório!")
+    @NotBlank(message = "O título é obrigatório")
     private String titulo;
     @NotBlank(message = "O autor é obrigatório")
     private String autor;
     @Enumerated(EnumType.STRING)
-    @NotNull(message = "A categoria é obrigatório")
+    @NotNull(message = "A categoria é obrigatória")
     private Categoria categoria;
     private String editora;
     @DecimalMin(value = "0.99", message = "O preço deve ser no mínimo 0.99")
     private BigDecimal preco;
-    @Pattern(regexp = "^970\\d(7)$|^970\\d{10}$",
+    @Pattern(regexp = "^970\\d{7}$|^970\\d{10}$",
             message = "ISBN fora do padrão")
     private String isbn;
     private LocalDate dataPublicacao;
