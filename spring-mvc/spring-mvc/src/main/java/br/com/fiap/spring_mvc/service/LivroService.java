c776aaad26bac28eef10d0074f1f90589c7bfaef
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-08-19 21:40:02 -0300

    feat: novo front-end implementado

--- spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/service/LivroService.java
+++ spring-mvc/spring-mvc/src/main/java/br/com/fiap/spring_mvc/service/LivroService.java
@@ -2,31 +2,38 @@ package br.com.fiap.spring_mvc.service;
 
 import br.com.fiap.spring_mvc.entity.Livro;
 import br.com.fiap.spring_mvc.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class LivroService {
-    LivroRepository livroRepository;
+    private final LivroRepository livroRepository;
+
+    @Autowired
     public LivroService(LivroRepository livroRepository) {
         this.livroRepository = livroRepository;
     }
-    //CRUD
 
+    // CRUD -> Create, Read, Update, Delete
     public Livro createLivro(Livro livro) {
         return livroRepository.save(livro);
+    }
 
+    public Livro readLivro(Long id) {
+        return livroRepository.findById(id).orElse(null);
     }
-    public Livro readLivro(Livro livro){
-        return livroRepository.findById(livro.getId()).orElse(null);
-    }
-    public List<Livro> readLivros(){
+
+    public List<Livro> readLivros() {
         return livroRepository.findAll();
-
     }
 
-    public void deleteLivro(Livro livro){
-        livroRepository.delete(livro);
+    public Livro updateLivro(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    public void deleteLivro(Long id) {
+        livroRepository.deleteById(id);
     }
 }
